10d99ff46ec5061d63e49e89269d12f169f0c0d6
David Stinnett <davidstinnett@icloud.com>
2021-01-21 12:41:49 -0500

    finished project

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ function App() {
 	const [text, setText] = useState([]);
 	const handleSubmit = (e) => {
 		e.preventDefault();
-
-		console.log("hello world");
+		let amount = parseInt(count);
+		if (count <= 0) {
+			amount = 1;
+		}
+		if (count > 8) {
+			amount = 8;
+		}
+		setText(data.slice(0, amount));
 	};
 
 	return (
@@ -26,14 +32,9 @@ function App() {
 				</button>
 			</form>
 			<article className="lorem-text">
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
-					assumenda.
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
-					assumenda.
-				</p>
+				{text.map((item, index) => {
+					return <p key={index}>{item}</p>;
+				})}
 			</article>
 		</section>
 	);
